d9b900d72c3f2e731419e6835d704f7d0812226f
johnsonmjacob <jacobmj88@gmail.com>
2024-12-19 08:24:12 -0800

    java script added

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+console.log("Welcome to my portfolio website!")
+function menuToggle () {
+    var x = document.getElementById('mynavtoggle');
+    if (x.classname == 'navtoggle'); {
+        x.classname == 'responsive' 
+    } else {
+        x.classname ='navtoggle';
+    } 
+}   
+    
